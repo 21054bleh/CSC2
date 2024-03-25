9479ad3a0e2f06058e888899a24b607e2dcc0daa
McCormick <21054@fraser.school.nz>
2024-03-25 14:28:46 +1300

    move ticket sold down

--- 02_looping.py
+++ 02_looping.py
@@ -8,14 +8,14 @@ tickets_sold = 0
 while tickets_sold < MAX_TICKETS:
     name = input("Please enter your name or 'xxx, to quit: ")
 
-    tickets_sold += 1
-
     if name == 'xxx':
         break
+
+    tickets_sold += 1
 
 # Output number of tickets sold
 if tickets_sold == MAX_TICKETS:
     print("congratulations you have sold all the tickets")
 
 else:
-    print("you have sole {} ticket/s.  There is {} ticket/s ramaining").format(tickets_sold, MAX_TICKETS - tickets_sold)
+    print(f"you have sole {tickets_sold} ticket/s.  There is {MAX_TICKETS - tickets_sold} ticket/s ramaining")
